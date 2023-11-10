7aaeca1d7beabf2f594b5cef2d2ad16efd305ae7
Karibash <y.karibayashi@gmail.com>
2023-11-10 19:01:39 +0900

    [feature] Make it output an error if the branch name does not match the rule

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as core from '@actions/core';
+import { minimatch } from 'minimatch';
+
 import type { Rule } from './type';
 
 export type MainOptions = {
@@ -9,8 +12,9 @@ export const main = ({
   head,
   rules,
 }: MainOptions) => {
-  console.log({
-    head,
-    rules,
-  });
+  const rule = rules.find(rule => minimatch(head, rule.head));
+  if (!rule) {
+    core.setFailed(`The branch name does not follow the rules: ${head}`);
+    return;
+  }
 };
